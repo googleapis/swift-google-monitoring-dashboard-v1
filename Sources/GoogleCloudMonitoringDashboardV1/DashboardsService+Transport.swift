f6d356a6c598c6b979c4ec2499358eebda3c2d9b
Carlos O'Ryan <coryan@google.com>
2026-09-11 19:52:59 +0000

    chore: update librarian with `setBody()` (#819)

--- Sources/GoogleCloudMonitoringDashboardV1/DashboardsService+Transport.swift
+++ Sources/GoogleCloudMonitoringDashboardV1/DashboardsService+Transport.swift
@@ -50,7 +50,7 @@ extension Clients {
       req.setMethod(.POST)
       req.addHeader(name: GoogleCloudGax._HeaderNames.apiClient, value: Clients.clientHeader)
       if let body = request.dashboard {
-        req.setBody(data: try JSONEncoder().encode(body), ofContentType: "application/json")
+        try req.setBody(json: body)
       }
       return try await req.rpc(
         GoogleCloudMonitoringDashboardV1.Dashboard.self, timeout: options.attemptTimeout
@@ -140,7 +140,7 @@ extension Clients {
       req.setMethod(.PATCH)
       req.addHeader(name: GoogleCloudGax._HeaderNames.apiClient, value: Clients.clientHeader)
       if let body = request.dashboard {
-        req.setBody(data: try JSONEncoder().encode(body), ofContentType: "application/json")
+        try req.setBody(json: body)
       }
       return try await req.rpc(
         GoogleCloudMonitoringDashboardV1.Dashboard.self, timeout: options.attemptTimeout
